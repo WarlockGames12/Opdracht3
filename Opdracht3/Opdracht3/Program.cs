dd48bff99cc4a07d63f96ace427513b39adbafe4
WarlockGames12 <fpweeyou@gmail.com>
2021-10-01 11:16:07 +0200

    Opdracht 3

---
+++ Opdracht3/Opdracht3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Opdracht3
+{
+
+    
+
+    public class Tile
+    {
+        private int heigh;
+        private int widt;
+
+        public Tile(int Tileheight, int Tilewidth)
+        {
+            widt = Tilewidth;
+            heigh = Tileheight;
+        }
+    }
+
+    public class Room
+    {
+
+        Tile[][] tile = new Tile[10][];
+
+        public Room(int height, int width)
+        {
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+        }
+
+        
+
+
+
+
+    }
+}
+    
+    
+
